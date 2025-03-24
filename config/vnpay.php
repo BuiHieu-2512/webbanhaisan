4cc124dcd6fcd7ae302d3d964583bda57d8369ba
BuiHieu-2512 <buihieu250312@gmail.com>
2025-03-24 16:02:35 +0700

    thembieudovagiaodiendangnhap

--- config/vnpay.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'vnp_TmnCode' => env('VNP_TMN_CODE'),
-    'vnp_HashSecret' => env('VNP_HASH_SECRET'),
-    'vnp_Url' => env('VNP_URL'),
-    'vnp_ReturnUrl' => env('VNP_RETURN_URL'),
-];
